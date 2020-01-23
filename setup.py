9c7225919018911eb3266512c96c49a03f2dc455
PaulinaFranco <dadiante@gmail.com>
2020-01-23 11:21:34 -0600

    Add license and change name

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='prueba',
+    name='scraper',
     version='0.0.1',
     description='Job searcher',
     py_modules=['scrape_jobs', 'print_all_jobs', 'prueba'],
     package_dir={'': 'src'},
+    clasiffiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "License :: MIT License"
+    ],
 )
