abf800fd405c5409620e7a331764fbd7ee3a3b3f
PaulinaFranco <dadiante@gmail.com>
2020-01-23 10:17:56 -0600

    Configuration

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='prueba',
+    version='0.0.1',
+    description='Job searcher',
+    py_modules=['scrape_jobs', 'print_all_jobs', 'prueba'],
+    package_dir={'': 'src'},
+)
